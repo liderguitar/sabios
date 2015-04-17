7c423cadc4c73202e13a4844bddc09d3312ce604
liderguitar <liderguitar@gmail.com>
2015-04-17 20:35:14 -0300

    superadmin

--- application/controllers/SuperadminController.php
+++ application/controllers/SuperadminController.php
@@ -5,4 +5,22 @@ class SuperadminController extends Zend_Controller_Action
 	{
 		$this->_helper->layout()->disableLayout();
 	}
+
+    public function listAction(){
+
+        $adapter = Doctrine_Query::create()->from('Application')->where("app_id <>  'home'")->execute();
+
+        $this->view->page = $this->_request->getParam('page');
+        //die($this->view->page);
+        $paginator = new Zend_Paginator($adapter);
+        $paginator->setItemCountPerPage(15);
+        $paginator->setCurrentPageNumber($this->view->page);
+
+        $this->view->objects = $paginator;
+
+    }
+
+
+
+
 }
